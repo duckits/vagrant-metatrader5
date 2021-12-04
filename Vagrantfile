07a7396e136e097f8fd05e20760a7663cd8d2b13
russell sherman <russell.t.sherman@gmail.com>
2021-12-04 14:00:46 -0600

    initial commit

---
+++ Vagrantfile
@@ -0,0 +1,31 @@
+# -*- mode: ruby -*-
+# vi: set ft=ruby :
+
+Vagrant.require_version ">= 1.6.2"
+
+Vagrant.configure("2") do |config|
+  # windows server 2019 core
+  # config.vm.box = "gusztavvargadr/windows-server"
+  # config.vm.box_version = "1809.0.1904"
+  config.vm.box = "windows-2022-amd64-virtualbox.box"
+
+  config.vm.provider "virtualbox" do |vb|
+    vb.name = "MetaTrader"
+
+    vb.gui = true
+
+    # Tweak these to fit your needs.
+    vb.memory = "10240"
+    vb.cpus = "4"
+  end
+
+  # share this project directory to c:\vagrant
+  config.vm.synced_folder "./", "c:/vagrant"
+
+  # copy our rsa key to guest
+  config.vm.provision "file", source: "~/.ssh/id_rsa", destination: "c:/Users/vagrant/.ssh/id_rsa"
+  config.vm.provision "file", source: "~/.ssh/id_rsa.pub", destination: "c:/Users/vagrant/.ssh/id_rsa.pub"
+  # this should allow for direct checkout of application to guest and pushing commits from guest to github
+
+  config.vm.provision "user-data", type: "shell", path: "provision.ps1", upload_path: "c:/vagrant/provision.ps1", privileged: true
+end
---
+++ provision.ps1
@@ -0,0 +1,63 @@
+Set-StrictMode -Version Latest
+$ProgressPreference = 'SilentlyContinue'
+$ErrorActionPreference = 'Stop'
+trap {
+    Write-Host
+    Write-Host 'ERROR: $_'
+    Write-Host (($_.ScriptStackTrace -split '\r?\n') -replace '^(.*)$','ERROR: $1')
+    Write-Host (($_.Exception.ToString() -split '\r?\n') -replace '^(.*)$','ERROR EXCEPTION: $1')
+    Write-Host
+    Write-Host 'Sleeping for 60m to give you time to look around the virtual machine before self-destruction...'
+    Start-Sleep -Seconds (60*60)
+    Exit 1
+}
+
+$hostName = "MetaTrader"
+
+Write-Host 'Enable auto logon...'
+Write-Host "###################################################################"
+$logonPath = 'HKLM:SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon'
+Set-ItemProperty -Path $logonPath -Name AutoAdminLogon -Value 1
+Set-ItemProperty -Path $logonPath -Name DefaultDomainName -Value $hostName
+Set-ItemProperty -Path $logonPath -Name DefaultUserName -Value vagrant
+Set-ItemProperty -Path $logonPath -Name DefaultPassword -Value vagrant
+
+Write-Host 'Show File Extensions'
+Write-Host "###################################################################"
+Set-ItemProperty `
+    -Path 'HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced' `
+    -Name 'HideFileExt' `
+    -Value 0
+
+$ScriptPath = Split-Path $MyInvocation.InvocationName
+
+Write-Host 'Install Chocolaty'
+Write-Host "###################################################################"
+Start-Process "powershell.exe" `
+  -ArgumentList "$ScriptPath\scripts\chocolaty_install.ps1" -Wait
+
+Write-Host 'Install Git'
+Write-Host "###################################################################"
+Start-Process "powershell.exe" `
+  -ArgumentList "$ScriptPath\scripts\git_install.ps1" -Wait
+
+Write-Host 'Install Metatrader 4'
+Write-Host "###################################################################"
+Start-Process -ArgumentList '/auto' `
+  -FilePath "$ScriptPath\mt4setup.exe" -Wait
+
+Write-Host 'Install Metatrader 5'
+Write-Host "###################################################################"
+Start-Process -ArgumentList '/auto' `
+  -FilePath "$ScriptPath\mt5setup.exe" -Wait
+
+Rename-Computer -NewName $hostName -Force
+
+Write-Host 'Restarting'
+Write-Host "###################################################################"
+Start-Process `
+  -ArgumentList '/c "timeout /t 3 /nobreak && shutdown -r -f -t 0"' `
+  -FilePath "cmd.exe" `
+  -WindowStyle Hidden
+
+Exit 0
