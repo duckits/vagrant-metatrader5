d337b5db6a31000ff5fcb3f815a8ec1cb51b027e
russell sherman <russell.t.sherman@gmail.com>
2021-12-23 15:08:16 -0600

    add multiple terminals with shared custom source directories

--- Vagrantfile
+++ Vagrantfile
@@ -25,10 +25,5 @@ Vagrant.configure("2") do |config|
   # mount metatrader source code project into c:\MQL
   config.vm.synced_folder "/Users/russellsherman/src/github.com/russelltsherman/metatrader", "c:/MQL"
 
-  # copy our rsa key to guest
-  config.vm.provision "file", source: "~/.ssh/id_rsa", destination: "c:/Users/vagrant/.ssh/id_rsa"
-  config.vm.provision "file", source: "~/.ssh/id_rsa.pub", destination: "c:/Users/vagrant/.ssh/id_rsa.pub"
-  # this should allow for direct checkout of application to guest and pushing commits from guest to github
-
   config.vm.provision "user-data", type: "shell", path: "provision.ps1", upload_path: "c:/vagrant/provision.ps1", privileged: true
 end
--- provision.ps1
+++ provision.ps1
@@ -31,30 +31,62 @@ Set-ItemProperty `
 
 $ScriptPath = Split-Path $MyInvocation.InvocationName
 
-Write-Host 'Install Chocolaty'
-Write-Host "###################################################################"
-Start-Process "powershell.exe" `
-  -ArgumentList "$ScriptPath\scripts\chocolaty.ps1" -Wait
+# mt4setup downloaded from metatrader official installs mt5
+# Write-Host 'Install MetaTrader 4'
+# Write-Host "###################################################################"
+# Start-Process -ArgumentList '/auto' `
+#   -FilePath "$ScriptPath\mt4setup.exe" -Wait
 
-Write-Host 'Install Git'
-Write-Host "###################################################################"
-Start-Process "powershell.exe" `
-  -ArgumentList "$ScriptPath\scripts\git.ps1" -Wait
-
-Write-Host 'Install Metatrader 4'
+Write-Host 'Install MetaTrader 4 (forexcom)'
 Write-Host "###################################################################"
 Start-Process -ArgumentList '/auto' `
-  -FilePath "$ScriptPath\mt4setup.exe" -Wait
+  -FilePath "$ScriptPath\forexcom4setup.exe" -Wait
 
-Write-Host 'Install Metatrader 5'
+Write-Host 'Install MetaTrader 4 (ig)'
+Write-Host "###################################################################"
+Start-Process -ArgumentList '/auto' `
+  -FilePath "$ScriptPath\ig4setup.exe" -Wait
+
+Write-Host 'Install MetaTrader 4 (oanda)'
+Write-Host "###################################################################"
+Start-Process -ArgumentList '/auto' `
+  -FilePath "$ScriptPath\oanda4setup.exe" -Wait
+
+Write-Host 'Install MetaTrader 5'
 Write-Host "###################################################################"
 Start-Process -ArgumentList '/auto' `
   -FilePath "$ScriptPath\mt5setup.exe" -Wait
 
-Write-Host 'Install Windows Subsystem Linux'
-Write-Host "###################################################################"
-Start-Process "powershell.exe" `
-  -ArgumentList "wsl --install" -Wait
+# before symlinking our custom metatrader code
+# we need to ensure the applications have been launched at least once
+# otherwise the expected directories won't exist.
+Start-Process -FilePath "C:/Program Files (x86)/FOREX.com US/terminal.exe"
+Start-Process -FilePath "C:/Program Files (x86)/IG MetaTrader 4 Terminal/terminal.exe"
+Start-Process -FilePath "C:/Program Files (x86)/OANDA - Metatrader/terminal.exe"
+Start-Process -FilePath "C:/Program Files/MetaTrader 5/terminal64.exe"
+
+Start-Sleep -Seconds 60 # pause a minute to let applications launch
+
+# get list of directories in c:/MQL
+$directories = Get-ChildItem c:/MQL/ |
+       Where-Object {$_.PSIsContainer} |
+       Foreach-Object {$_.Name}
+
+for ($i=0; $i -lt $directories.length; $i++) {
+  $folder = $directories[$i]
+  New-Item -ItemType SymbolicLink `
+    -Path "C:/Program Files (x86)/FOREX.com US/MQL4/$folder/Custom" `
+    -Target "c:/MQL/$folder"
+  New-Item -ItemType SymbolicLink `
+    -Path "C:/Program Files (x86)/IG MetaTrader 4 Terminal/MQL4/$folder/Custom" `
+    -Target "c:/MQL/$folder"
+  New-Item -ItemType SymbolicLink `
+    -Path "C:/Program Files (x86)/OANDA - Metatrader/MQL4/$folder/Custom" `
+    -Target "c:/MQL/$folder"
+  New-Item -ItemType SymbolicLink `
+    -Path "C:/Program Files/MetaTrader 5/MQL5/$folder/Custom" `
+    -Target "c:/MQL/$folder"
+}
 
 Rename-Computer -NewName $hostName -Force
 
